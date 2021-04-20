b7dddb5cdd11fccab5ba58de12060c7f50937b3f
tf153 <43797501+tf153@users.noreply.github.com>
2021-04-20 21:27:16 +0530

    good night

--- DP/knap sack/recursive_count_subset_given_sum.cpp
+++ DP/knap sack/recursive_count_subset_given_sum.cpp
@@ -5,14 +5,13 @@ int t[100][100];
 
 int count_subset(int arr[], int sum, int n)
 {
-    if (n == 0 || sum == 0)
-        return 0;
     if (t[n][sum] != -1)
         return t[n][sum];
+    if (n == 0 || sum == 0)
+        return 0;
     if (arr[n - 1] <= sum)
-    {
         return t[n][sum] = count_subset(arr, sum - arr[n - 1], n - 1) + count_subset(arr, sum, n - 1);
-    }
+
     return t[n][sum] = count_subset(arr, sum, n - 1);
 }
 
@@ -25,11 +24,13 @@ int main()
         cin >> arr[i];
     int sum;
     cin >> sum;
+
     memset(t, -1, sizeof(t));
     for (int i = 0; i <= n; i++)
         t[i][0] = 1;
     for (int i = 1; i <= sum; i++)
         t[0][i] = 0;
+
     cout << count_subset(arr, sum, n);
     return 0;
 }
